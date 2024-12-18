2bbcfbb040f68ed48364a193b55e8fc49de10af7
4345rtd <162415455+4345rtd@users.noreply.github.com>
2024-12-18 23:39:49 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[[ -f /.dockerenv || ! -z $VIRTUAL_ENV ]] || { echo "[ERROR] You want to be installing this in a virtual environment. Did you call this script standalone?"; exit 1; }
+[[ -z $VIRTUAL_ENV ]] || { echo "[ERROR] You want to be installing this in a virtual environment. Did you call this script standalone?"; exit 1; }
 
 # 1. install fuzzware (excluding angr)
 echo "[*] Installing pipeline..."
